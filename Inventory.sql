9fcff0b31cb540f83c12643611087d1970947700
nhungly2805 <128270865+nhungly2805@users.noreply.github.com>
2024-04-23 14:14:06 +0700

    Add files via upload

---
+++ Inventory.sql
@@ -0,0 +1,258 @@
+﻿-- Check for data validation
+SELECT *
+FROM purchase_2017
+WHERE brand_id IS NULL OR description IS NULL OR purchase_price IS NULL 
+	OR size IS NULL OR volume IS NULL OR classification IS NULL OR
+	purchase_price IS NULL OR vendor_name IS NULL OR vendor_number IS NULL
+
+SELECT *
+FROM inventory_begin
+WHERE inventory_id IS NULL OR store_id IS NULL OR city_name IS NULL 
+	OR brand_id IS NULL OR description IS NULL OR size IS NULL OR
+	on_hand IS NULL OR invent_price IS NULL OR start_date IS NULL
+
+SELECT *
+FROM inventory_end
+WHERE inventory_id IS NULL OR store_id IS NULL OR city_name IS NULL 
+	OR brand_id IS NULL OR description IS NULL OR size IS NULL OR
+	on_hand IS NULL OR invent_price IS NULL OR end_date IS NULL
+
+SELECT *
+FROM invoice_purchase
+WHERE vendor_number IS NULL OR vendor_name IS NULL OR invoice_date IS NULL 
+	OR po_number IS NULL OR po_date IS NULL OR pay_date IS NULL OR
+	quantity IS NULL OR dollars IS NULL OR freight IS NULL OR approval IS NULL
+
+SELECT *
+FROM purchase_2016
+WHERE inventory_id IS NULL OR store_id IS NULL OR brand_id IS NULL OR
+	description IS NULL OR size IS NULL or vendor_number IS NULL OR
+	vendor_name IS NULL OR po_number IS NULL OR po_date IS NULL OR 
+	receiving_date IS NULL OR pay_date IS NULL OR purchase_price IS NULL OR
+	quantity IS NULL OR dollars IS NULL OR classification IS NULL
+
+SELECT *
+FROM sales_2016
+WHERE inventory_id IS NULL OR store_id IS NULL OR brand_id IS NULL OR
+	description IS NULL OR size IS NULL OR description IS NULL OR
+	sales_quantity IS NULL OR sales_dollars IS NULL OR sales_price IS NULL OR
+	sales_date IS NULL OR volume IS NULL OR classification IS NULL OR
+	excise_tax IS NULL OR vendor_no IS NULL OR vendor_name IS NULL
+
+-- EDA for each table
+--- EDA for begin inventory
+SELECT COUNT(*) as total_observation, COUNT(DISTINCT(inventory_id)) as number_inventory_id, 
+		COUNT(DISTINCT(store_id)) AS number_store, COUNT(DISTINCT(city_name)) as number_city, 
+		COUNT(DISTINCT(brand_id)) AS number_brand, COUNT(DISTINCT(description)) as number_description, 
+		MAX(invent_price) as max_price, MIN(invent_price) as min_price, AVG(invent_price) as avg_invent_price, 
+		MAX(on_hand) as max_on_hand, MIN(on_hand) as min_on_hand, AVG(on_hand) as avg_on_hand
+FROM inventory_begin
+
+SELECT *
+FROM inventory_begin
+WHERE invent_price = 0
+
+SELECT *
+FROM inventory_end
+WHERE brand_id = 19138
+
+SELECT brand_id, COUNT(DISTINCT(description))
+FROM inventory_begin
+GROUP BY brand_id
+HAVING COUNT(DISTINCT(description)) != 1
+
+SELECT b.brand_id, b.description, p.vendor_name
+FROM inventory_begin b
+INNER JOIN (SELECT description, COUNT(DISTINCT(brand_id)) as number_description
+FROM inventory_begin 
+GROUP BY description
+HAVING COUNT(DISTINCT(brand_id)) > 1) c
+ON b.description = c.description
+LEFT JOIN purchase_2016 p
+ON p.brand_id = b.brand_id
+GROUP BY b.brand_id, b.description, p.vendor_name
+ORDER BY b.description
+
+SELECT b.description, p.vendor_name
+FROM inventory_begin b
+LEFT JOIN purchase_2016 p
+ON p.brand_id = b.brand_id
+WHERE b.description IN (SELECT b.description
+						FROM inventory_begin b
+						INNER JOIN (SELECT description, COUNT(DISTINCT(brand_id)) as number_description
+						FROM inventory_begin 
+						GROUP BY description
+						HAVING COUNT(DISTINCT(brand_id)) > 1) c
+						ON b.description = c.description
+						LEFT JOIN purchase_2016 p
+						ON p.description = b.description
+						GROUP BY b.description
+						HAVING COUNT(DISTINCT(p.vendor_name)) > 1
+						)
+GROUP BY b.description, p.vendor_name
+ORDER BY b.description
+
+--- EDA for ending inventory
+SELECT COUNT(*) as total_observation, COUNT(DISTINCT(inventory_id)) as number_inventory_id, 
+		COUNT(DISTINCT(store_id)) AS number_store, COUNT(DISTINCT(city_name)) as number_city, 
+		COUNT(DISTINCT(brand_id)) AS number_brand, COUNT(DISTINCT(description)) as number_description, 
+		MAX(invent_price) as max_price, MIN(invent_price) as min_price, AVG(invent_price) as avg_invent_price, 
+		MAX(on_hand) as max_on_hand, MIN(on_hand) as min_on_hand, AVG(on_hand) as avg_on_hand
+FROM inventory_end
+
+SELECT b.store_id as begin_store_id, b.city_name as begin_city_name, e.store_id as end_store_id, e.city_name as end_city_name
+FROM (SELECT store_id, city_name
+FROM inventory_begin
+GROUP BY store_id, city_name) b
+FULL JOIN
+(SELECT store_id, city_name
+FROM inventory_end
+GROUP BY store_id, city_name) e
+ON b.store_id = e.store_id AND b.store_id = e.store_id
+WHERE (e.city_name != b.city_name) OR b.city_name IS NULL OR  e.city_name IS NULL
+ORDER BY b.store_id
+
+--- EDA for purchase 2016
+SELECT COUNT(*) as total_observation, COUNT(DISTINCT(inventory_id)) as number_inventory_id, 
+		COUNT(DISTINCT(store_id)) AS number_store, COUNT(DISTINCT(brand_id)) AS number_brand, 
+		COUNT(DISTINCT(description)) as number_description, MAX(purchase_price) as max_price, 
+		MIN(purchase_price) as min_price, AVG(purchase_price) as avg_purchase_price
+FROM purchase_2016
+
+SELECT *
+FROM purchase_2016
+WHERE purchase_price = 0
+
+SELECT vendor_number, vendor_name, SUM(quantity) as total_quantity, SUM(dollars) as total_dollars
+FROM purchase_2016
+WHERE vendor_number = 2561
+GROUP BY vendor_number, vendor_name
+
+SELECT vendor_number, vendor_name, SUM(quantity) as total_quantity, SUM(dollars) as total_dollars
+FROM invoice_purchase
+WHERE vendor_number = 2561
+GROUP BY vendor_number, vendor_name
+
+SELECT *
+FROM inventory_end
+WHERE brand_id = 2166
+
+--- EDA for sales 2016
+SELECT COUNT(*) as total_observation, COUNT(DISTINCT(inventory_id)) as number_inventory_id, 
+		COUNT(DISTINCT(store_id)) AS number_store, COUNT(DISTINCT(brand_id)) AS number_brand, 
+		COUNT(DISTINCT(description)) as number_description, MAX(sales_price) as max_price, MIN(sales_price) as min_price,
+		 AVG(sales_price) as avg_sales_price, MAX(sales_date) as max_sales_date, MIN(excise_tax) as min_tax
+FROM sales_2016
+
+SELECT *
+FROM sales_2016
+WHERE store_id = 81
+
+--- EDA for invoice purchase
+SELECT COUNT(*) as total_observation, COUNT(DISTINCT(vendor_name)) AS number_vendor_name,
+	COUNT(DISTINCT(vendor_number)) AS number_vendor, MAX(invoice_date) AS max_invoice_date,
+	MIN(invoice_date) AS min_invoice_date, MIN(quantity) AS min_quantity, MAX(quantity) AS max_quantity,
+	MIN(dollars) AS min_purchase, MAX(dollars) AS max_purchase, MIN(freight) AS min_freight,
+	MAX(freight) AS max_freight
+FROM invoice_purchase
+WHERE YEAR(invoice_date) = 2016
+
+SELECT vendor_number, vendor_name, SUM(dollars) as total_dollars, SUM(freight) as total_freight, MAX(dollars) as max_dollars, MAX(freight) as max_freight
+FROM invoice_purchase
+WHERE vendor_number IN 
+(SELECT vendor_number
+FROM invoice_purchase
+GROUP BY vendor_number
+HAVING COUNT(DISTINCT(vendor_name)) > 1) AND YEAR(invoice_date) = 2016
+GROUP BY vendor_number, vendor_name 
+
+-- Analyse inventory
+--- Beginning
+SELECT TOP 10 description, SUM(on_hand) as begin_on_hand
+FROM inventory_begin
+GROUP BY description
+ORDER BY begin_on_hand DESC
+
+--- Ending
+SELECT TOP 10 description, SUM(on_hand) as end_on_hand
+FROM inventory_end
+GROUP BY description
+ORDER BY end_on_hand DESC
+
+-- Analyse sales
+--- Sales by description
+SELECT TOP 10 description, SUM(sales_quantity) as total_sales_quantity
+FROM sales_2016
+GROUP BY description
+ORDER BY total_sales_quantity DESC
+
+--- Sales by store_id
+SELECT TOP 5 store_id, ROUND(SUM(dollars_after_tax),2) as total_sales
+FROM sales_2016
+GROUP BY store_id
+ORDER BY total_sales DESC
+
+SELECT TOP 5 store_id, ROUND(SUM(dollars_after_tax),2) as total_sales
+FROM sales_2016
+GROUP BY store_id
+ORDER BY total_sales
+
+--- Sales by month
+SELECT MONTH(sales_date) as month, ROUND(SUM(dollars_after_tax),2) as total_sales
+FROM sales_2016
+GROUP BY MONTH(sales_date)
+ORDER BY total_sales
+
+--- Sales by city
+SELECT TOP 5 e.city_name, ROUND(SUM(dollars_after_tax),2) as total_sales 
+FROM sales_2016 s
+LEFT JOIN inventory_end e
+ON s.store_id = e.store_id
+GROUP BY city_name
+ORDER BY total_sales DESC
+
+SELECT TOP 5 e.city_name, ROUND(SUM(dollars_after_tax),2) as total_sales 
+FROM sales_2016 s
+LEFT JOIN inventory_end e
+ON s.store_id = e.store_id
+GROUP BY city_name
+ORDER BY total_sales
+
+-- Analyse purchases
+--- Purchase by quantity
+SELECT TOP 5 vendor_name, SUM(quantity) as total_quantity
+FROM purchase_2016
+GROUP BY vendor_name
+ORDER BY total_quantity DESC
+--- Purchase by cost
+SELECT TOP 5 vendor_name, ROUND(SUM(dollars),2) as total_dollars
+FROM purchase_2016
+GROUP BY vendor_name
+ORDER BY total_dollars DESC
+
+
+WITH sales_2016_group AS
+(SELECT description, SUM(sales_quantity) AS total_sales_quantity, 
+		ROUND(SUM(dollars_after_tax),2) AS total_dollars_after_tax
+FROM sales_2016
+GROUP BY description),
+
+purchase_2016_group AS
+(SELECT description, SUM(quantity) AS total_purchase
+FROM purchase_2016
+WHERE YEAR(po_date) = 2016 AND month(po_date) <= 2
+GROUP BY description)
+
+SELECT p.description, total_sales_quantity, total_purchase, total_dollars_after_tax,
+	SUM(total_dollars_after_tax) OVER() AS total_dollars,
+	ROUND(total_dollars_after_tax / SUM(total_dollars_after_tax) OVER() *100, 2) AS percent_total,
+	DENSE_RANK() OVER(ORDER BY total_dollars_after_tax DESC) AS product_rank_by_revenue
+FROM purchase_2016_group p
+LEFT JOIN sales_2016_group s
+ON p.description = s.description
+ORDER BY total_dollars_after_tax DESC
+
+SELECT *
+FROM abc_group
+WHERE store_id = 3
+ORDER BY store_id, total_dollars_after_tax DESC
---
+++ Inventory_create_temp_table.sql
@@ -0,0 +1,224 @@
+﻿UPDATE [InvoicePurchases12312016]
+SET ["VendorName"] = CONCAT(["VendorName"], ["InvoiceDate"]), 
+	["InvoiceDate"] = ["PONumber"], 
+	["PONumber"] = ["PODate"],
+	["PODate"] = ["PayDate"],
+	["PayDate"] = ["Quantity"],
+	["Quantity"] = ["Dollars"],
+	["Dollars"] = ["Freight"],
+	["Freight"] = LEFT(["Approval"], CHARINDEX(',', ["Approval"]) - 1),
+	["Approval"] =  RIGHT(["Approval"], LEN(["Approval"]) - CHARINDEX(',', ["Approval"]))
+WHERE ["VendorNumber"] IN (3950, 2, 7240, 8664)
+
+UPDATE [PurchasesFINAL12312016]
+	SET ["VendorName"] = CONCAT(["VendorName"], ["PONumber"]), 
+	["PONumber"] = ["PODate"],
+	["PODate"] = ["ReceivingDate"],
+	["ReceivingDate"] = ["InvoiceDate"],
+	["InvoiceDate"] = ["PayDate"],
+	["PurchasePrice"] = ["Quantity"],
+	["Quantity"] = ["Dollars"],
+	["Dollars"] = LEFT(["Classification"], CHARINDEX(',', ["Classification"]) - 1),
+	["Classification"] =  RIGHT(["Classification"], LEN(["Classification"]) - CHARINDEX(',', ["Classification"]))
+	WHERE ["VendorNumber"] IN (3950, 2, 7240, 8664)
+
+-- Xu ly du lieu o table '2017PurchasePricesDec'
+SELECT CAST(["Brand"] AS INT) AS brand_id,
+		["Price"] AS price, 
+		TRIM(REPLACE(["Description"],'"','')) AS description,
+		TRIM(REPLACE(["Size"],'"','')) AS size,
+		TRIM(REPLACE(["Volume"],'"','')) AS volume,
+		["Classification"] AS classification,
+		["PurchasePrice"] AS purchase_price,
+		TRIM(REPLACE(["VendorName"],'"','')) AS vendor_name,
+		["VendorNumber"] AS vendor_number
+INTO purchase_2017
+FROM [2017PurchasePricesDec]
+
+-- Xu ly du lieu o table 'BegInvFINAL12312016'
+SELECT TRIM(REPLACE(["InventoryId"],'"','')) AS inventory_id,
+		CAST(["Store"] AS INT) AS store_id, 
+		TRIM(REPLACE(["City"],'"','')) AS city_name,
+		CAST(["Brand"] AS INT) AS brand_id,
+		TRIM(REPLACE(["Description"],'"','')) AS description,
+		TRIM(REPLACE(["Size"],'"','')) AS size,
+		CAST(["onHand"] AS INT) AS on_hand,
+		CAST(["Price"] AS FLOAT) AS invent_price,
+		CAST(["startDate"] AS DATE) AS start_date
+INTO inventory_begin
+FROM [BegInvFINAL12312016]
+
+-- Tương tự xử lý dữ liệu ở table 'EndInvFINAL12312016'
+SELECT TRIM(REPLACE(["InventoryId"],'"','')) AS inventory_id,
+		CAST(["Store"] AS INT) AS store_id, 
+		TRIM(REPLACE(["City"],'"','')) AS city_name,
+		CAST(["Brand"] AS INT) AS brand_id,
+		TRIM(REPLACE(["Description"],'"','')) AS description,
+		TRIM(REPLACE(["Size"],'"','')) AS size,
+		CAST(["onHand"] AS INT) AS on_hand,
+		CAST(["Price"] AS FLOAT) AS invent_price,
+		CAST(["endDate"] AS DATE) AS end_date
+INTO inventory_end
+FROM [EndInvFINAL12312016]
+	
+SELECT CAST(["VendorNumber"] AS INT) AS vendor_number,
+		TRIM(REPLACE(["VendorName"],'"','')) AS vendor_name, 
+		CONVERT(DATE,REPLACE(["InvoiceDate"],'"','')) AS invoice_date,
+		CAST(["PONumber"] AS INT) AS po_number,
+		CONVERT(DATE,REPLACE(["PODate"],'"','')) AS po_date,
+		CONVERT(DATE,REPLACE(["PayDate"],'"','')) AS pay_date,
+		CAST(["Quantity"] AS INT) AS quantity,
+		CAST(["Dollars"] AS FLOAT) AS dollars,
+		CAST(["Freight"] AS FLOAT) AS freight,
+		TRIM(REPLACE(["Approval"],'"','')) AS approval
+INTO invoice_purchase
+FROM [InvoicePurchases12312016]
+
+SELECT TRIM(REPLACE(["InventoryId"],'"','')) AS inventory_id,
+			CAST(["Store"] AS INT) AS store_id,
+			CAST(["Brand"] AS INT) AS brand_id,
+			TRIM(REPLACE(["Description"],'"','')) AS description,
+			TRIM(REPLACE(["Size"],'"','')) AS size,
+			CAST(["VendorNumber"] AS INT) AS vendor_number,
+			TRIM(REPLACE(["VendorName"],'"','')) AS vendor_name,
+			CAST(["PONumber"] AS INT) AS po_number,
+			CONVERT(DATE,REPLACE(["PODate"],'"','')) AS po_date,
+			CONVERT(DATE,REPLACE(["ReceivingDate"],'"','')) AS receiving_date,
+			CONVERT(DATE,REPLACE(["InvoiceDate"],'"','')) AS invoice_date,
+			CONVERT(DATE,REPLACE(["PayDate"],'"','')) AS pay_date,
+			CAST(["PurchasePrice"] AS FLOAT) AS purchase_price,
+			CAST(["Quantity"] AS INT) AS quantity,
+			CAST(["Dollars"] AS FLOAT) AS dollars,
+			CAST(["Classification"] AS INT) AS classification
+INTO purchase_2016
+FROM PurchasesFINAL12312016
+
+SELECT TRIM(InventoryId) AS inventory_id,
+			CAST(Store AS INT) AS store_id,
+			CAST(Brand AS INT) AS brand_id,
+			TRIM(Description) AS description,
+			TRIM(Size) AS size,
+			CAST(SalesQuantity AS INT) AS sales_quantity,
+			CAST(SalesDollars AS FLOAT) AS sales_dollars,
+			CAST(SalesPrice AS FLOAT) AS sales_price,
+			CAST(SalesDate AS date) AS sales_date,
+			CAST(Volume AS INT) AS volume,
+			CAST(Classification AS INT) AS classification,
+			CAST(ExciseTax AS FLOAT) AS excise_tax,
+			CAST(VendorNo AS INT) AS vendor_no,
+			TRIM(VendorName) AS vendor_name
+INTO sales_2016
+FROM [SalesFINAL12312016]
+
+--- Calculate lead time
+ALTER TABLE purchase_2016
+ADD lead_time INT NULL
+
+UPDATE purchase_2016
+SET lead_time = DATEDIFF(day, po_date, receiving_date)
+
+--- Calculate sales after excise_tax
+
+ALTER TABLE sales_2016
+ADD dollars_after_tax FLOAT NULL
+
+UPDATE sales_2016
+SET dollars_after_tax = sales_dollars - excise_tax
+
+-- Update some values
+UPDATE inventory_begin
+SET invent_price = 9.99
+WHERE brand_id = 19138
+
+UPDATE inventory_end
+SET city_name = 'TYWARDREATH'
+WHERE store_id = 46
+
+UPDATE purchase_2016
+SET purchase_price = 54.99, dollars = ROUND(54.99*quantity,2)
+WHERE brand_id = 2166
+
+UPDATE invoice_purchase
+SET vendor_name = 'VINEYARD BRANDS INC'
+WHERE vendor_number = 1587
+
+UPDATE invoice_purchase
+SET vendor_name = 'SOUTHERN GLAZERS WINE AND SPIRITS'
+WHERE vendor_number = 2000
+
+UPDATE purchase_2016
+SET vendor_name = 'VINEYARD BRANDS INC'
+WHERE vendor_number = 1587
+
+UPDATE purchase_2016
+SET vendor_name = 'SOUTHERN GLAZERS WINE AND SPIRITS'
+WHERE vendor_number = 2000
+
+-- Prepare some metrics
+WITH sales_2016_group AS
+(SELECT store_id, description, ROUND(AVG(sales_quantity),0) AS avg_sales_quantity, SUM(sales_quantity) AS total_sales_quantity, 
+		ROUND(SUM(dollars_after_tax),2) AS total_dollars_after_tax, ROUND(MAX(sales_price),2) AS sales_per, COUNT(*) OVER() as total
+FROM sales_2016
+GROUP BY store_id, description),
+
+purchase_2016_group AS
+(SELECT store_id, description, ROUND(AVG(lead_time),0) AS avg_lead_time, SUM(quantity) AS total_purchase, 
+		ROUND(MAX(purchase_price),2) AS purchase_per, COUNT(*) OVER() as total
+FROM purchase_2016
+GROUP BY store_id, description),
+
+inventory_begin_group AS
+(SELECT store_id, description, SUM(on_hand) AS on_hand_begin, COUNT(*) OVER() as total
+FROM inventory_begin 
+GROUP BY store_id, description),
+
+inventory_end_group AS
+(SELECT store_id, description, SUM(on_hand) AS on_hand_end, COUNT(*) OVER() as total
+FROM inventory_end 
+GROUP BY store_id, description)
+
+SELECT total_group.description, total_group.store_id, ISNULL(total_group.on_hand_end,0) AS on_hand_end, 
+		ISNULL(total_group.on_hand_begin,0) as on_hand_begin, ISNULL(total_group.avg_lead_time,0) as avg_lead_time,
+		ISNULL(total_group.avg_sales_quantity,0) as avg_sales_quantity, ISNULL(total_group.reorder_point,0) as reorder_point, 
+		CASE WHEN (total_group.reorder_point - total_group.on_hand_end) > 0 THEN (total_group.reorder_point - total_group.on_hand_end)
+					       ELSE 0 END as order_quantity, 
+		ISNULL(total_group.total_sales_quantity,0) as total_sales_quantity, ISNULL(total_group.total_purchase,0) as total_purchase, 
+		ISNULL(total_group.sales_per,0) as sales_per, ISNULL(total_group.purchase_per,0) as purchase_per, 
+		ISNULL(total_group.total_dollars_after_tax,0) as total_dollars_after_tax, 
+		total_group.cumulative_total_dollars, total_group.total_dollars_store, total_group.cumulative_percent, 
+		total_group.product_rank_by_revenue, total_group.class
+INTO abc_group
+FROM
+(SELECT abc.description, abc.store_id, e.on_hand_end AS on_hand_end, b.on_hand_begin AS on_hand_begin, avg_lead_time, avg_sales_quantity, 
+		CEILING(avg_lead_time*avg_sales_quantity + (CASE WHEN class = 'A' THEN 0.3
+												WHEN class = 'B' THEN 0.2
+												ELSE 0.1 END)*avg_lead_time*avg_sales_quantity) AS reorder_point,
+		total_sales_quantity, total_purchase, sales_per, purchase_per, ROUND(total_dollars_after_tax,2) AS total_dollars_after_tax, 
+		ROUND(cumulative_total_dollars,2) AS cumulative_total_dollars, ROUND(total_dollars,2) AS total_dollars_store, 
+		ROUND(cumulative_percent,2) AS cumulative_percent, product_rank_by_revenue, class
+FROM inventory_end_group e
+RIGHT JOIN
+(SELECT p.description, p.store_id, avg_lead_time, avg_sales_quantity, total_sales_quantity, total_purchase, sales_per, 
+	purchase_per, total_dollars_after_tax,
+	SUM(total_dollars_after_tax) OVER(PARTITION BY p.store_id ORDER BY total_dollars_after_tax DESC) AS cumulative_total_dollars,
+	SUM(total_dollars_after_tax) OVER(PARTITION BY p.store_id) AS total_dollars,
+	SUM(total_dollars_after_tax) OVER(PARTITION BY p.store_id ORDER BY total_dollars_after_tax DESC) / SUM(total_dollars_after_tax) OVER(PARTITION BY p.store_id) AS cumulative_percent,
+	DENSE_RANK() OVER(PARTITION BY p.store_id ORDER BY total_dollars_after_tax DESC) AS product_rank_by_revenue,
+	CASE 
+		WHEN SUM(total_dollars_after_tax) OVER(PARTITION BY p.store_id ORDER BY total_dollars_after_tax DESC) / SUM(total_dollars_after_tax) OVER(PARTITION BY p.store_id) <= 0.7 
+			THEN 'A'
+		WHEN SUM(total_dollars_after_tax) OVER(PARTITION BY p.store_id ORDER BY total_dollars_after_tax DESC) / SUM(total_dollars_after_tax) OVER(PARTITION BY p.store_id) <= 0.9 
+			THEN 'B'
+		ELSE 'C'
+	END AS class
+FROM purchase_2016_group p
+LEFT JOIN sales_2016_group s
+ON p.description = s.description
+AND p.store_id = s.store_id
+WHERE p.store_id != 81) abc
+ON abc.description = e.description 
+AND abc.store_id = e.store_id
+LEFT JOIN inventory_begin_group b
+ON b.description = abc.description 
+AND abc.store_id = b.store_id
+) total_group
